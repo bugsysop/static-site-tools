c7707d09471fb3ae6bc72484aecce932449768f6
Aris <aris@papatheodorou.net>
2020-12-18 00:02:21 +0100

    Basic instructions in comments

--- hugo_metrics.sh
+++ hugo_metrics.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-#Make executable: chmod u+x hugo_metrics.sh
+# hugo_metrics.sh [17.12.2020]
+# Output Templates Metrics for this project
+# Copy at root of Hugo project
+# Make executable: chmod u+x hugo_metrics.sh
+# Enjoy
 ./bin/hugo --templateMetrics
