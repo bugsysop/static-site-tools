223cb7202413965370d6c9adde42f34e2d9b048b
Aris <aris@papatheodorou.net>
2020-12-18 00:01:36 +0100

    Basic instructions in comments

--- hugo_serve.sh
+++ hugo_serve.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-#Make executable: chmod u+x hugo_serve.sh
+# hugo_serve.sh [17.12.2020]
+# Serve site localy with Hugo server
+# Copy at root of Hugo project
+# Make executable: chmod u+x hugo_metrics.sh
+# Enjoy
 ./bin/hugo server --disableFastRender
