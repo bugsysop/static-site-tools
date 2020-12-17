e670070934cad959b02fedbaa85c662771953824
Aris <aris@papatheodorou.net>
2020-12-18 00:03:38 +0100

    Remove existing public/ directory before build

--- hugo_build.sh
+++ hugo_build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# hugo_build.sh [17.12.2020]
+# Remove ./public directory if exist then buil site for production
+# Copy at root of Hugo project
 # Make executable: chmod u+x hugo_build.sh
+# Enjoy
+output_dir=./public
+[ -d "$output_dir" ] && rm -fr "$output_dir"
 ./bin/hugo --minify --gc
